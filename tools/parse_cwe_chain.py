9b38308dc74bfd0161b00099049748cfa4da0e53
Martin Prpič <mprpic@redhat.com>
2020-11-10 09:07:10 -0500

    Add tools to experiment with CWE data

--- tools/parse_cwe_chain.py
+++ tools/parse_cwe_chain.py
@@ -1,16 +1,24 @@
-cwe_definitions = {}
+import argparse
+import json
 
 
-def fetch_cwe_definitions():
-    pass
+def load_cwe_definitions():
+    """Load CWE definitions from a local JSON file.
+
+    This data can be fetched with get_cwe_defs.py.
+    """
+    with open("./cwe_full_defs.json") as f:
+        data = json.load(f)
+    return data
 
 
-def cwe_text(cwe_string):
+def create_cwe_text(cwe_string):
     """Create a text representation of a CWE string along with a verbose translation.
 
     For example, "CWE-190->CWE-122" becomes: "CWE-190->CWE-122: Integer Overflow or
     Wraparound leads to Heap-based Buffer Overflow".
     """
+    cwe_defs = load_cwe_definitions()
     cwe_parts = cwe_string.split("->")
     final_parts = []
     for part in cwe_parts:
@@ -19,16 +27,34 @@ def cwe_text(cwe_string):
                 # Only one level of nesting (using parens) is allowed so strip them all and split
                 # to get all individual CWEs that need to be ORed.
                 cwe_or = part.strip("()").split("|")
-                cwe_or = " or ".join(cwe_definitions[cwe] for cwe in cwe_or)
+                cwe_or = " or ".join(cwe_defs[cwe] for cwe in cwe_or)
                 final_parts.append(cwe_or)
             else:
-                cwe = cwe_definitions[part.strip("()")]
+                cwe = cwe_defs[part.strip("()")]
                 final_parts.append(cwe)
 
         except KeyError:
             # If a CWE does not exist, return only the CWE string since showing an incomplete
             # CWE description text is worse than showing none at all.
-            return cwe_string
+            print(f"CWE(s) not found in: {part}")
+            return
 
     desc = " leads to ".join(final_parts)
-    return f"{self.cwe}: {desc}"
+    return f"{cwe_string}: {desc}"
+
+
+def main():
+    p = argparse.ArgumentParser(description="Display CWE text ")
+    p.add_argument("cwe_string", help="print CWE definitions")
+    args = p.parse_args()
+
+    if args.cwe_string:
+        cwe_text = create_cwe_text(args.cwe_string)
+        if cwe_text:
+            print(cwe_text)
+    else:
+        p.print_help()
+
+
+if __name__ == "__main__":
+    main()
